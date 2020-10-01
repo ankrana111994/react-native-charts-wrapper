b3fdb9d11bd4c4fd3fa3b356f66958c848c6c90a
ank111994 <65998504+ank111994@users.noreply.github.com>
2020-10-01 18:19:50 +0530

    Update DrawableUtils.java

--- android/src/main/java/com/github/wuxudong/rncharts/utils/DrawableUtils.java
+++ android/src/main/java/com/github/wuxudong/rncharts/utils/DrawableUtils.java
@@ -3,6 +3,9 @@ package com.github.wuxudong.rncharts.utils;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
@@ -14,9 +17,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class DrawableUtils {
-    public static Drawable drawableFromUrl(String url, final int width, final int height) {
+    public static Drawable drawableFromUrl(String url, final int width, final int height, String color) {
         try {
-            return new DrawableLoadingAsyncTask().execute(url, Integer.toString(width), Integer.toString(height)).get();
+            return new DrawableLoadingAsyncTask().execute(url, Integer.toString(width), Integer.toString(height), color).get();
         } catch (Exception e) {
             // draw dummy drawable when execution fail
             e.printStackTrace();
@@ -38,14 +41,41 @@ public class DrawableUtils {
                 InputStream input = connection.getInputStream();
 
                 x = BitmapFactory.decodeStream(input);
+                Bitmap bitmap = x;
 
-                return new BitmapDrawable(Resources.getSystem(), Bitmap.createScaledBitmap(x, width, height, true));
 
-            } catch(IOException e) {
+                int padding = 40;
+                int strokeWidth = 6;
+
+                int bitmapSize = Math.max(bitmap.getWidth(), bitmap.getHeight()) + padding + strokeWidth;
+                Bitmap workingBitmap = Bitmap.createBitmap(bitmapSize, bitmapSize,
+                        Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(workingBitmap);
+
+                Paint paint = new Paint();
+                paint.setAntiAlias(true);
+                paint.setColor(Color.parseColor(strings[3]));
+                paint.setStyle(Paint.Style.STROKE);
+
+                paint.setStrokeWidth(strokeWidth);
+
+                canvas.drawBitmap(bitmap, (bitmapSize - bitmap.getWidth()) / 2.0f,
+                        (bitmapSize - bitmap.getHeight()) / 2.0f, paint);
+
+                int centerCoordinate = bitmapSize / 2;
+
+                canvas.drawCircle(centerCoordinate, centerCoordinate,
+                        centerCoordinate - (strokeWidth / 2.0f), paint);
+
+                return new BitmapDrawable(Resources.getSystem(), Bitmap.createScaledBitmap(workingBitmap, width, height, true));
+
+            } catch (IOException e) {
                 e.printStackTrace();
                 // draw dummy drawable when connection fail
                 return new ShapeDrawable();
             }
         }
-    };
+    }
+
+    ;
 }
